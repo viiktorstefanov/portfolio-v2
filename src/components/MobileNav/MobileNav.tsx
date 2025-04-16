4ff4c86a6bbbcec98cd9925e1f0b6654127e37be
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 20:11:20 +0300

    adding mobile nav component

---
+++ src/components/MobileNav/MobileNav.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import SandwichIcon from "../SandwichIcon/SandwichIcon";
+import MobileMenu from "../MobileMenu/MobileMenu";
+import LinkedInIcon from "../LinkedInIcon/LinkedInIcon";
+import GithubIcon from "../GithubIcon/GithubIcon";
+
+const MobileNav: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const onClick = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  return (
+    <div className="flex justify-between items-center h-[60px] z-[1] w-full px-6 max-w-[1200px]">
+      <div className="flex justify-center items-center gap-5">
+        <GithubIcon />
+        <LinkedInIcon />
+      </div>
+      <SandwichIcon onClick={onClick} />
+      {openMenu && <MobileMenu onClick={onClick} />}
+    </div>
+  );
+};
+
+export default MobileNav;
