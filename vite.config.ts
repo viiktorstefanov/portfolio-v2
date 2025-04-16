ca8ddbb83cf43a6324656310e62d274dd57dbb4a
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 20:10:10 +0300

    removing shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
-import path from "path";
-import react from "@vitejs/plugin-react"
-
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
