996e977c7d4ad4e9bfe96c5d4b386ebfa3073c27
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 20:11:10 +0300

    adding mobile menu icon

---
+++ src/components/SandwichIcon/SandwichIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+
+type SandwichIconProps = {
+    onClick: () => void;
+}
+
+const SandwichIcon: React.FC<SandwichIconProps> = ({ onClick }) => {
+  return (
+    <div
+    className="block absolute top-3 right-0 transform -translate-x-full translate-y-3/5 cursor-pointer text-inherit text-[1.5rem]"
+    onClick={onClick}
+  >
+      <FaBars className='text-inherit'/>
+    </div>
+  );
+};
+
+export default SandwichIcon;
