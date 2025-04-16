5ca03a9f39a7b2bbfba3dabcbc270f1a28099b95
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 20:11:48 +0300

    adding desktop nav comp

--- src/components/DesktopNav/DesktopNav.tsx
+++ src/components/DesktopNav/DesktopNav.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import GithubButton from "../GithubButton/GithubButton";
+import LinkedInIcon from "../LinkedInIcon/LinkedInIcon";
+import { menuItems } from "../../constants/data";
+import MenuItem from "../MenuItem/MenuItem";
 
 const DesktopNav: React.FC = () => {
   return (
-    <div>
-      desktop nav
+    <div className="w-full flex items-center justify-center gap-5 px-[6px] text-[18px] max-w-[1200px]">
+      <div className="w-[70%] flex items-center justify-start gap-8 px-1.5 text-lg text-white">
+        {menuItems.map((item) => (
+          <MenuItem key={item.label} target={item.href} label={item.label} />
+        ))}
+      </div>
+
+      <div className="w-[30%] h-full flex justify-end items-center px-[6px] py-0 gap-5">
+        <LinkedInIcon className="text-white" size={30} />
+        <GithubButton />
+      </div>
     </div>
   );
 };
