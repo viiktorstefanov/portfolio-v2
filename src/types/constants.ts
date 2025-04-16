50232953fe0ba6b1750192a4609b1bc23514f8ff
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 10:35:11 +0300

    adding data types

---
+++ src/types/constants.ts
@@ -0,0 +1,36 @@
+export type BioType = {
+  name: string;
+  roles: string[];
+  description: string;
+  github: string;
+  linkedin: string;
+};
+
+export type SkillItem = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+export type SkillCategory = {
+  id: number;
+  title: string;
+  skills: SkillItem[];
+};
+
+export type Experience = {
+  id: number;
+  img: string;
+  role: string;
+  company: string;
+  date: string;
+};
+
+export type Education = {
+  id: number;
+  img: string;
+  school: string;
+  date: string;
+  desc: string;
+  degree: string;
+};
