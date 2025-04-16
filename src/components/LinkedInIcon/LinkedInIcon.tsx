c2a69e3fc36efb3d0a177b46fb5e7d50a323f43a
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 20:12:21 +0300

    adding linkedin icon comp

---
+++ src/components/LinkedInIcon/LinkedInIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { Bio } from "../../constants/data";
+import { Link } from "react-router-dom";
+
+type LinkedInIconProps = {
+  className?: string;
+  size?: number;
+};
+
+const LinkedInIcon: React.FC<LinkedInIconProps> = ({ className, size = 24 }) => {
+  return (
+    <Link to={Bio.linkedin} target="_blank" className="hover:opacity-50">
+      <BsLinkedin size={size} className={className} />
+    </Link>
+  );
+};
+
+export default LinkedInIcon;
