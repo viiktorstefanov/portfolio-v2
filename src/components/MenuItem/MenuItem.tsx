48b17502d1397e4a06fcffa9cf29e294d0280f41
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-16 20:12:27 +0300

    adding menu item comp

---
+++ src/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { HashLink } from 'react-router-hash-link';
+
+type MenuItemProps = {
+    target: string,
+    label: string,
+    onClick?: () => void;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ target, label, onClick }) => {
+  return (
+    <HashLink className='font-medium hover:text-primary-app transition duration-200 ease-in-out' onClick={onClick} smooth to={target}>{label}</HashLink>
+  );
+};
+
+export default MenuItem;
