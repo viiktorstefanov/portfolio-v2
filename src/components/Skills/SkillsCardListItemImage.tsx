e96ec41c27598a8611b7f2b97e541ee997a3d5ae
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 23:55:23 +0300

    adding Skills card list item image component

---
+++ src/components/Skills/SkillsCardListItemImage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type SkillsCardListItemImageProps = {
+  url: string;
+  alt: string;
+};
+
+const SkillsCardListItemImage: React.FC<SkillsCardListItemImageProps> = ({
+  url,
+  alt,
+}) => {
+  return <img src={url} alt={alt} className="h-[24px] w-[24px]" />;
+};
+
+export default SkillsCardListItemImage;
