c3181dc7ceabfd676c5a30180897e4bb7d0d01cf
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 23:55:11 +0300

    adding Skills card list item component

---
+++ src/components/Skills/SkillsCardListItem.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type SkillsCardListItemProps = {
+  children: React.ReactNode;
+};
+
+const SkillsCardListItem: React.FC<SkillsCardListItemProps> = ({
+  children,
+}) => {
+  return (
+    <div
+      className="text-white text-[16px] font-normal rounded-[12px] px-4 py-3 flex items-center justify-center gap-2 
+md:text-[16px] md:px-3 md:py-2 
+max-[500px]:text-[14px] max-[500px]:px-3 max-[500px]:py-[6px]"
+    >
+      {children}
+    </div>
+  );
+};
+
+export default SkillsCardListItem;
