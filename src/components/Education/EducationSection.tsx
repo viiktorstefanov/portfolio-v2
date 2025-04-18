6e4357245cbbf0779be8619183110c5e8ac3a4f9
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 22:45:32 +0300

    adding padding to education section

--- src/components/Education/EducationSection.tsx
+++ src/components/Education/EducationSection.tsx
@@ -8,7 +8,7 @@ type EducationSectionProps = {
 const EducationSection: React.FC<EducationSectionProps> = ({ id, children }) => {
   return (
     <section
-      className="h-[50dvh] flex flex-col justify-center items-center relative z-[1] pb-[60px] max-[960px]:pb-0"
+      className="flex flex-col justify-center items-center pt-20 relative z-[1] pb-[60px] max-[960px]:pb-0"
       id={id}
     >
         {children}
