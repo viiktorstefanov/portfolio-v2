a0277eeff0344b19c95d0448ca13d809721c8b5d
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 22:51:07 +0300

    edit experience component style

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -3,7 +3,7 @@ import { menuItems } from '../../constants/data';
 
 const Experience: React.FC = () => {
   return (
-    <div className='flex flex-col justify-center relative z-[1] items-center py-[40px] pb-[80px] md:py-0' id={menuItems[2].href}>
+    <div className='flex flex-col justify-center relative z-[1] items-center py-[40px] pb-[80px] md:py-0 h-[300px]' id={menuItems[2].href}>
       
     </div>
   );
