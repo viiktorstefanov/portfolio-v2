0c3db776304171c0a4fddb30deede7be545a5487
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 23:54:04 +0300

    adding Skills component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { menuItems } from '../../constants/data';
+import React from "react";
+import { menuItems, skills } from "../../constants/data";
+import Wrapper from "./SkillsWrapper";
+import Section from "./SkillsSection";
+import Title from "./SkillsTitle";
+import Container from "./SkillsContainer";
+import SkillsCard from "./SkillsCard";
 
 const Skills: React.FC = () => {
+  const skillsInfo = menuItems.find((item) => item.label === "Skills")!;
+
   return (
-    <div className='flex flex-col justify-center relative z-10 items-center pt-20' id={menuItems[1].href}>
-      
-    </div>
+    <Section id={skillsInfo.href}>
+      <Wrapper>
+        <Title />
+        <Container>
+          {skills.map((skill, index) => (
+            <SkillsCard key={skill.id} skill={skill} index={index} />
+          ))}
+        </Container>
+      </Wrapper>
+    </Section>
   );
 };
 
