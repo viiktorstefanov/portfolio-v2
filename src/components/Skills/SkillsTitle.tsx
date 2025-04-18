ff70730b7e6ff969f54516f5d25e631e2027515a
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 23:54:22 +0300

    adding Skills title component

---
+++ src/components/Skills/SkillsTitle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SkillsTitle: React.FC = () => {
+  return (
+    <h2 className="font-poppins text-[32px] md:text-[42px] text-center font-semibold mt-3 md:mt-5 text-white">
+      Skills
+    </h2>
+  );
+};
+
+export default SkillsTitle;
