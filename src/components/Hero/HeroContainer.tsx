fa6b2e6a2a80ba7b095b137bb7097713568ff40b
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-18 22:50:13 +0300

    adding hero container

---
+++ src/components/Hero/HeroContainer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type HeroContainerProps = {
+  children: React.ReactNode;
+};
+
+const HeroContainer: React.FC<HeroContainerProps> = ({ children }) => {
+  return (
+    <div className="clip-hero-polygon flex justify-center relative z-[1] px-[30px] py-[80px] md:px-[30px] md:py-[80px] max-md:px-16 max-md:py-[66px] max-[640px]:px-16 max-[640px]:py-[32px]">
+      {children}
+    </div>
+  );
+};
+
+export default HeroContainer;
