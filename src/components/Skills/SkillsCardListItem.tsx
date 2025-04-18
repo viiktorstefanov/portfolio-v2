e99896117ac946072b5580b92bc614cf5ac68dd6
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-19 00:02:05 +0300

    edit Skills card list item styles

--- src/components/Skills/SkillsCardListItem.tsx
+++ src/components/Skills/SkillsCardListItem.tsx
@@ -10,7 +10,7 @@ const SkillsCardListItem: React.FC<SkillsCardListItemProps> = ({
   return (
     <div
       className="text-white text-[16px] font-normal rounded-[12px] px-4 py-3 flex items-center justify-center gap-2 
-md:text-[16px] md:px-3 md:py-2 
+md:text-[16px] md:px-4 md:py-3 
 max-[500px]:text-[14px] max-[500px]:px-3 max-[500px]:py-[6px]"
     >
       {children}
