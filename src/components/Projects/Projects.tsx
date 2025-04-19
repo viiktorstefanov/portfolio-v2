44f1f5819b5061f9834543189c61f07e0cfb6a29
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-19 18:15:13 +0300

    adding Projects component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { menuItems } from '../../constants/data';
+import React, { useMemo, useState } from "react";
+import { menuItems, projects } from "../../constants/data";
+import Section from "./ProjectsSection";
+import Wrapper from "./ProjectsWrapper";
+import Title from "./ProjectsTitle";
+import Container from "./ProjectsContainer";
+import ProjectsModal from "./ProjectsModal";
+import { Project } from "../../types/constants";
 
 const Projects: React.FC = () => {
+  const projectsInfo = menuItems.find((item) => item.label === "Projects")!;
+  const [openModal, setOpenModal] = useState<{
+    state: boolean;
+    id: string | null;
+  }>({ state: false, id: null });
+
+  const onOpenModal = (id: string) => {
+    setOpenModal({ state: true, id });
+  };
+
+  const onCloseModal = () => {
+    setOpenModal({ state: false, id: null });
+  };
+
+  const selectedProject = useMemo(() => {
+    return openModal.id
+      ? projects.find((proj: Project) => proj.id === openModal.id)
+      : null;
+  }, [openModal.id]);
+
   return (
-    <div className='flex flex-col justify-center relative z-[1] items-center pt-20' id={menuItems[3].href}>
-      
-    </div>
+    <Section id={projectsInfo.href}>
+      <Wrapper>
+        <Title title={projectsInfo.label} />
+        <Container projects={projects} onOpenModal={onOpenModal} />
+        {selectedProject && (
+          <ProjectsModal
+            openModal={openModal.state}
+            onCloseModal={onCloseModal}
+            project={selectedProject}
+          />
+        )}
+      </Wrapper>
+    </Section>
   );
 };
 
