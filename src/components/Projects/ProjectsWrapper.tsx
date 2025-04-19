59e498c385c3ee978a1b6d5b999d0a8a7b9ce1c7
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-19 18:15:27 +0300

    adding Projects wrapper component

---
+++ src/components/Projects/ProjectsWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type ProjectsWrapperProps = {
+  children: React.ReactNode;
+};
+
+const ProjectsWrapper: React.FC<ProjectsWrapperProps> = ({ children }) => {
+  return (
+    <div className="font-poppins relative flex flex-col justify-between items-center w-full max-w-[1350px] py-[10px] pb-[100px] gap-[12px] md:flex-col">
+      {children}
+    </div>
+  );
+};
+
+export default ProjectsWrapper;
