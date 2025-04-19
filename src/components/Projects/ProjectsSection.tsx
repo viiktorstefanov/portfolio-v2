bcfe2f7874d020b96126d6d5044580a2f5f645d2
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-19 18:15:35 +0300

    adding Projects section component

---
+++ src/components/Projects/ProjectsSection.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ProjectsSectionProps = {
+  children: React.ReactNode;
+  id: string;
+};
+
+const ProjectsSection: React.FC<ProjectsSectionProps> = ({ children, id }) => {
+  return (
+    <section
+      id={id}
+      className="clip-projects-polygon flex flex-col justify-center relative z-[1] items-center pt-20"
+    >
+      {children}
+    </section>
+  );
+};
+
+export default ProjectsSection;
