0d5e66dae7539eafbbbe5f18a10f82cd348e03e5
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-19 12:26:11 +0300

    adding Experience wrapper component

---
+++ src/components/Experience/ExperienceWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type ExperienceWrapperProps = {
+  children: React.ReactNode;
+};
+
+const ExperienceWrapper: React.FC<ExperienceWrapperProps> = ({ children }) => {
+  return (
+    <div className="relative flex flex-col justify-between items-center w-full max-w-[1350px] py-[55px] gap-[12px]">
+      {children}
+    </div>
+  );
+};
+
+export default ExperienceWrapper;
