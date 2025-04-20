73c43128460d8f8bdc708f790045269d7ea7f231
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-20 16:28:28 +0300

    edit experience wrapper style

--- src/components/Experience/ExperienceWrapper.tsx
+++ src/components/Experience/ExperienceWrapper.tsx
@@ -6,7 +6,7 @@ type ExperienceWrapperProps = {
 
 const ExperienceWrapper: React.FC<ExperienceWrapperProps> = ({ children }) => {
   return (
-    <div className="relative flex flex-col justify-between items-center w-full max-w-[1350px] py-[55px] gap-[12px]">
+    <div className="relative flex flex-col justify-between items-center w-full max-w-[1350px] py-[55px] gap-[12px] pb-36">
       {children}
     </div>
   );
