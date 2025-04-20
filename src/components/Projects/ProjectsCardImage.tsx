7f791b25942bc0aee26412ecc649c574d88ed0b8
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-20 16:30:10 +0300

    adding project card image component

--- src/components/Projects/ProjectsCardImage.tsx
+++ src/components/Projects/ProjectsCardImage.tsx
@@ -11,6 +11,7 @@ const ProjectsCardImage: React.FC<ProjectsCardImageProps> = ({ url, alt }) => {
       src={url}
       alt={alt}
       className="w-full h-[180px] bg-white rounded-[10px] shadow-[0_0_16px_2px_rgba(0,0,0,0.3)] object-cover"
+      loading="lazy"
     />
   );
 };
