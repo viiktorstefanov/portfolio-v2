ff9e664e9f6b30d1ef3a4775c04b4c40145d955e
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-21 23:19:38 +0300

    edit card image styles

--- src/components/Projects/ProjectsCardImage.tsx
+++ src/components/Projects/ProjectsCardImage.tsx
@@ -3,15 +3,31 @@ import React from "react";
 type ProjectsCardImageProps = {
   url: string;
   alt: string;
-  objectFit: string,
+  objectFit: ObjectFit;
 };
 
-const ProjectsCardImage: React.FC<ProjectsCardImageProps> = ({ url, alt, objectFit }) => {
+const objectFitMap = {
+  contain: "object-contain",
+  cover: "object-cover",
+  fill: "object-fill",
+  none: "object-none",
+  "scale-down": "object-scale-down",
+} as const;
+
+type ObjectFit = keyof typeof objectFitMap;
+
+const ProjectsCardImage: React.FC<ProjectsCardImageProps> = ({
+  url,
+  alt,
+  objectFit,
+}) => {
+  const objectFitClass = objectFitMap[objectFit];
+
   return (
     <img
       src={url}
       alt={alt}
-      className={`w-full h-[180px] bg-white rounded-[10px] shadow-[0_0_16px_2px_rgba(0,0,0,0.3)] object-${objectFit} border-2 border-solid border-black`}
+      className={`w-full h-[180px] bg-white rounded-[10px] shadow-[0_0_16px_2px_rgba(0,0,0,0.3)] ${objectFitClass} border-2 border-solid border-black`}
     />
   );
 };
