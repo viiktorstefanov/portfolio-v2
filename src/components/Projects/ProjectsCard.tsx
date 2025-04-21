540f6fac0cd6e250525813448605a73b96e6b223
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-21 22:37:53 +0300

    edit project card

--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -21,7 +21,7 @@ const ProjectsCard: React.FC<ProjectsCardProps> = ({
       className="w-[330px] h-[450px] bg-[#171721] cursor-pointer rounded-[10px] shadow-[0_0_12px_4px_rgba(0,0,0,0.4)] overflow-hidden p-[26px_20px] flex flex-col gap-[14px] transition-all duration-500 ease-in-out hover:-translate-y-[10px] hover:shadow-[0_0_50px_4px_rgba(0,0,0,0.6)] hover:brightness-110"
       onClick={() => onOpenModal(project.id)}
     >
-      <Image url={project.image} alt={project.title} />
+      <Image url={project.image.url} alt={project.title} objectFit={project.image.objectFit} />
       <Tags>
         {project.tags.map((tag, index) => (
           <Tag key={index} tag={tag} />
