a07090f837cd5287b750115497d1463537d1f427
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-21 16:21:29 +0300

    remove lazy loading img

--- src/components/Projects/ProjectsCardImage.tsx
+++ src/components/Projects/ProjectsCardImage.tsx
@@ -11,7 +11,6 @@ const ProjectsCardImage: React.FC<ProjectsCardImageProps> = ({ url, alt }) => {
       src={url}
       alt={alt}
       className="w-full h-[180px] bg-white rounded-[10px] shadow-[0_0_16px_2px_rgba(0,0,0,0.3)] object-cover"
-      loading="lazy"
     />
   );
 };
