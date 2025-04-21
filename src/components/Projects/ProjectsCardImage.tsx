3cb6001fae0db06be03eebe279d9fa67ce31ec07
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-21 22:38:09 +0300

    edit project card image

--- src/components/Projects/ProjectsCardImage.tsx
+++ src/components/Projects/ProjectsCardImage.tsx
@@ -3,14 +3,15 @@ import React from "react";
 type ProjectsCardImageProps = {
   url: string;
   alt: string;
+  objectFit: string,
 };
 
-const ProjectsCardImage: React.FC<ProjectsCardImageProps> = ({ url, alt }) => {
+const ProjectsCardImage: React.FC<ProjectsCardImageProps> = ({ url, alt, objectFit }) => {
   return (
     <img
       src={url}
       alt={alt}
-      className="w-full h-[180px] bg-white rounded-[10px] shadow-[0_0_16px_2px_rgba(0,0,0,0.3)] object-cover border-2 border-solid border-black"
+      className={`w-full h-[180px] bg-white rounded-[10px] shadow-[0_0_16px_2px_rgba(0,0,0,0.3)] object-${objectFit} border-2 border-solid border-black`}
     />
   );
 };
