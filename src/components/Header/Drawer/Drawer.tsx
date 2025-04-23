24ff87e15ff323368360004f657acddff59096f9
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-23 16:24:51 +0300

    addin changing styles to tailwind

--- src/components/Header/Drawer/Drawer.tsx
+++ src/components/Header/Drawer/Drawer.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import DrawerMUI from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import { menuItems } from "../../../constants/data";
-import MenuItem from "../MenuItem/MenuItem";
-import { CloseRounded } from "@mui/icons-material";
-import { Link } from "react-scroll";
+import DrawerMenuItem from "./DrawerMenuItem";
+import DrawerCloseIcon from "./DrawerCloseIcon";
 
 type DrawerProps = {
   position: "top" | "right" | "bottom" | "left";
@@ -16,35 +15,21 @@ const Drawer: React.FC<DrawerProps> = ({ position, state, toggleDrawer }) => {
   return (
     <DrawerMUI anchor={position} open={state} onClose={toggleDrawer}>
       <Box
-        sx={{
-          width: 250,
-          backgroundColor: "#000",
-          height: "100%",
-          color: "white",
-        }}
-        className="font-poppins"
+        className="font-poppins w-[250px] bg-black h-full text-white"
         role="presentation"
         onClick={toggleDrawer}
         onKeyDown={toggleDrawer}
       >
         <div className="flex flex-col gap-3 cursor-pointer">
-          <div className="w-full flex items-end justify-end text-end pr-[5%] pt-[10%]">
-            <CloseRounded className="cursor-pointer" onClick={toggleDrawer} />
-          </div>
+          <DrawerCloseIcon onClick={toggleDrawer} />
           {menuItems.map((item) => (
-            <Link
-              to={item.href}
+            <DrawerMenuItem
               key={item.id}
-              className="flex gap-3 justify-start items-center pt-[10px] pr-0 pb-[10px] pl-[27px] "
-            >
-              <item.icon className="text-[25px] text-primary-app" />
-              <MenuItem
-                key={item.id}
-                target={item.href}
-                label={item.label}
-                onClick={toggleDrawer}
-              />
-            </Link>
+              target={item.href}
+              onClick={toggleDrawer}
+              icon={item.icon}
+              label={item.label}
+            />
           ))}
         </div>
       </Box>
