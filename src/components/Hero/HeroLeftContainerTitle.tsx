01463e67a5f6cdda40d3cbcbc2b4437fa6f4b109
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-24 14:12:41 +0300

    edit hero container title comp

--- src/components/Hero/HeroLeftContainerTitle.tsx
+++ src/components/Hero/HeroLeftContainerTitle.tsx
@@ -4,12 +4,12 @@ import { Bio } from "../../constants/data";
 const HeroLeftContainerTitle: React.FC = () => {
   return (
     <h2
-      className="font-poppins font-bold text-[50px] leading-[68px] text-white  
+      className="font-poppins font-bold text-4xl leading-[68px] text-white  
             max-[960px]:text-center 
-            max-[640px]:text-[40px] max-[640px]:leading-[48px] max-[640px]:mb-2"
+            max-[640px]:leading-[48px] max-[640px]:mb-2"
     >
       {" "}
-      Hi, I am <br /> {Bio.name}
+       {Bio.name}
     </h2>
   );
 };
