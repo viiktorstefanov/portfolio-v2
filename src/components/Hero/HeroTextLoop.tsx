c9d14570586950c2fe2f4cf28f2d842e814cf0ee
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-24 14:12:03 +0300

    edit hero text loop component mobile style

--- src/components/Hero/HeroTextLoop.tsx
+++ src/components/Hero/HeroTextLoop.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Bio } from "../../constants/data";
 import Typewriter from "typewriter-effect";
 import HeroSpan from "./HeroSpan";
+import useIsMobile from "../../hooks/useIsMobile";
 
 const HeroTextLoop: React.FC = () => {
+  const isMobile = useIsMobile();
+
+  if(isMobile) return null;
+  
   return (
     <div
       className="font-poppins font-semibold text-[32px] flex gap-3 text-white leading-[68px] 
            max-[960px]:text-center 
-           max-[640px]:text-[22px] max-[640px]:leading-[48px] max-[640px]:mb-4"
+           max-[640px]:text-[18px] max-[640px]:leading-[48px] max-[640px]:mb-4"
     >
       I am a
       <HeroSpan>
