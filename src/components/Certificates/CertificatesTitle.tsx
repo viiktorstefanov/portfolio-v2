884732cff5f0f395c75c6fb45d4a680ddaf6828c
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-26 19:37:46 +0300

    edit certificates title style

--- src/components/Certificates/CertificatesTitle.tsx
+++ src/components/Certificates/CertificatesTitle.tsx
@@ -6,7 +6,7 @@ type CertificatesTitleProps = {
 
 const CertificatesTitle: React.FC<CertificatesTitleProps> = ({ label }) => {
   return (
-    <h1 className="text-[42px] text-white text-center font-semibold mt-5 mb-[50px] md:text-[42px] md:mt-5 md:mb-[50px] sm:mt-3 sm:text-[32px]">
+    <h1 className="font-poppins text-center font-semibold text-white text-4xl md:text-5xl m-0">
       {label}
     </h1>
   );
