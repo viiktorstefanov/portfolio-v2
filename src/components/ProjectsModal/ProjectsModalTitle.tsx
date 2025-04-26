950616fd0922f4f4c9da3233edd500fdbff4c8ca
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-26 20:10:20 +0300

    edit project modal title styles

--- src/components/ProjectsModal/ProjectsModalTitle.tsx
+++ src/components/ProjectsModal/ProjectsModalTitle.tsx
@@ -6,7 +6,7 @@ type ProjectsModalTitleProps = {
 
 const ProjectsModalTitle: React.FC<ProjectsModalTitleProps> = ({ title }) => {
   return (
-    <h2 className="text-[28px] font-semibold text-white mt-2 mx-[6px] sm:mt-[6px]">
+    <h2 className="text-2xl md:text-3xl font-semibold text-white mt-2 mx-[6px] sm:mt-[6px]">
       {title}
     </h2>
   );
