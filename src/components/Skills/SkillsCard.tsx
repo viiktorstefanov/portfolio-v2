1cf032b4ec703703cdacff0d35020cf98968824e
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-26 20:09:54 +0300

    edit skills card styles

--- src/components/Skills/SkillsCard.tsx
+++ src/components/Skills/SkillsCard.tsx
@@ -14,9 +14,8 @@ type SkillsCardProps = {
 const SkillsCard: React.FC<SkillsCardProps> = ({ index, skill }) => {
   return (
     <motion.div
-      className="font-poppins w-full max-w-[500px] bg-[#171721e0] shadow-[0px_4px_24px_rgba(23,92,230,0.15)] rounded-2xl px-9 py-[18px]
-  md:max-w-[500px] md:px-9 md:py-[18px]
-  sm:max-w-[330px] sm:px-9 sm:py-[10px]"
+      className="font-poppins w-full bg-[#171721e0] shadow-[0px_4px_24px_rgba(23,92,230,0.15)] rounded-2xl  py-[18px]
+  md:max-w-[500px] sm:max-w-[330px]"
       whileInView={{ opacity: 1, y: 0 }}
       initial={{ opacity: 0, y: 120 }}
       transition={{
