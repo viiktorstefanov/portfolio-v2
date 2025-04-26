74c6766dc478b401f8fec0c44c6ea8ef6e8b4309
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-26 20:11:26 +0300

    edit project modal tag styles

--- src/components/ProjectsModal/ProjectModalTag.tsx
+++ src/components/ProjectsModal/ProjectModalTag.tsx
@@ -6,7 +6,7 @@ type ProjectModalTagProps = {
 
 const ProjectModalTag: React.FC<ProjectModalTagProps> = ({ tag }) => {
   return (
-    <span className="text-primary-app text-[14px] font-normal m-1 p-1 px-2 rounded-lg bg-[#2D2524] md:text-[14px] sm:text-[12px]">
+    <span className="text-primary-app text-xs md:text-sm font-normal m-1 p-1 px-2 rounded-lg bg-[#2D2524]">
       {tag}
     </span>
   );
