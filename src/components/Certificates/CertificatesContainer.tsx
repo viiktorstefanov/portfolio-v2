fb74df64fdf20f856cdb6dfeb5034e0d6b6d115b
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-26 19:37:34 +0300

    adding margin to certificates container

--- src/components/Certificates/CertificatesContainer.tsx
+++ src/components/Certificates/CertificatesContainer.tsx
@@ -8,7 +8,7 @@ const CertificatesContainer: React.FC<CertificatesContainerProps> = ({
   children,
 }) => {
   return (
-    <div className="flex justify-center items-center gap-7 flex-wrap">
+    <div className="flex justify-center items-center gap-7 flex-wrap mt-16">
       {children}
     </div>
   );
