3e41cdc1c1e65a5975b6095dde1679880b3b4ce9
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-26 20:11:04 +0300

    edit porject modal wrapper styles

--- src/components/ProjectsModal/ProjectModalWrapper.tsx
+++ src/components/ProjectsModal/ProjectModalWrapper.tsx
@@ -6,7 +6,7 @@ type ProjectModalWrapperProps = {
 
 const ProjectModalWrapper: React.FC<ProjectModalWrapperProps> = ( { children } ) => {
   return (
-    <div className='max-w-[800px] w-full rounded-2xl my-[50px] mx-[12px] h-min bg-[#171721] text-[#FFF] p-5 flex flex-col relative'>
+    <div className='max-w-[800px] w-full rounded-2xl my-[50px] mx-[12px] h-min bg-[#171721] text-[#FFF] p-5 flex flex-col gap-3 relative'>
       {children}
     </div>
   );
