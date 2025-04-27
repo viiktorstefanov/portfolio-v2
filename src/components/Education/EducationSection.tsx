f0ad9a02817dd0d08a53794dccc1679adc9fc47a
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:38:45 +0300

    edit education section styles

--- src/components/Education/EducationSection.tsx
+++ src/components/Education/EducationSection.tsx
@@ -8,7 +8,7 @@ type EducationSectionProps = {
 const EducationSection: React.FC<EducationSectionProps> = ({ id, children }) => {
   return (
     <section
-      className="flex flex-col justify-center items-center pt-20 relative z-[1] pb-[60px] max-[960px]:pb-0"
+      className="flex flex-col justify-center items-center pt-20 relative z-[1] pb-[60px] max-[960px]:pb-0 md:mt-20"
       id={id}
     >
         {children}
