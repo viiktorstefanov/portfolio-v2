74f3f29994368a58fd8389e743d8db0e9294cc37
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:38:27 +0300

    edit certificates wrapper style

--- src/components/Certificates/CertificatesWrapper.tsx
+++ src/components/Certificates/CertificatesWrapper.tsx
@@ -10,7 +10,7 @@ const CertificatesWrapper: React.FC<CertificatesWrapperProps> = ({
   return (
     <div
       className="relative flex flex-col justify-between items-center w-full max-w-[1350px] px-0 pt-[10px] pb-[100px] gap-3
-            md:flex-col"
+            md:flex-col md:mt-20"
     >
       {children}
     </div>
