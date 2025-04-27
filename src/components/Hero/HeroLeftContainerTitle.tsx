b15d1d68995b3b8dba56b6a88c4ef33b0f19ba9a
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:41:18 +0300

    edit hero left container title styles

--- src/components/Hero/HeroLeftContainerTitle.tsx
+++ src/components/Hero/HeroLeftContainerTitle.tsx
@@ -4,7 +4,7 @@ import { Bio } from "../../constants/data";
 const HeroLeftContainerTitle: React.FC = () => {
   return (
     <h2
-      className="font-poppins font-bold text-4xl leading-[68px] text-white  
+      className="font-poppins font-bold text-4xl md:text-[50px] leading-[68px] text-white  
             max-[960px]:text-center 
             max-[640px]:leading-[48px] max-[640px]:mb-2"
     >
