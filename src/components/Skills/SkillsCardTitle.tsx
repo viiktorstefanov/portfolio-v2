71b21a7fba98db408c497d9b998fe09e1a1d2f91
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:42:04 +0300

    edit skills card title styles

--- src/components/Skills/SkillsCardTitle.tsx
+++ src/components/Skills/SkillsCardTitle.tsx
@@ -6,7 +6,7 @@ type SkillsCardTitleProps = {
 
 const SkillsCardTitle: React.FC<SkillsCardTitleProps> = ({ title }) => {
   return (
-    <h2 className="text-2xl font-semibold text-white mb-8 text-center md:text-[28px]">
+    <h2 className="text-2xl font-semibold text-white mb-8 md:mb-9 text-center md:text-[28px] md:mt-4">
       {title}
     </h2>
   );
