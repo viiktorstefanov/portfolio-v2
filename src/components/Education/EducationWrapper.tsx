1d07b9f114b187a8899b4c3e8a2ef4ebf13235db
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:39:17 +0300

    edit education wrapper comp

--- src/components/Education/EducationWrapper.tsx
+++ src/components/Education/EducationWrapper.tsx
@@ -6,7 +6,7 @@ type EducationWrapperProps = {
 
 const EducationWrapper: React.FC<EducationWrapperProps> = ({ children }) => {
   return (
-    <div className="w-full max-w-[1000px] flex flex-col items-center justify-center gap-3 sm:items-center">
+    <div className="w-full max-w-[1000px] flex flex-col items-center justify-center gap-3 sm:items-center md:mt-14">
       {children}
     </div>
   );
