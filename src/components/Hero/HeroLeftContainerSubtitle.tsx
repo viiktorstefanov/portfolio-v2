75e73a955dac02104210548417638912f8cc3167
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:40:55 +0300

    edit hero left container subtitle styles

--- src/components/Hero/HeroLeftContainerSubtitle.tsx
+++ src/components/Hero/HeroLeftContainerSubtitle.tsx
@@ -5,8 +5,8 @@ import { Bio } from "../../constants/data";
 const HeroLeftContainerSubtitle: React.FC = () => {
   return (
     <div
-      className="font-poppins text-[20px] leading-[32px] pt-7 text-center sm:mb-[42px] text-[#b1b2b3]  
-           sm:text-[20px] sm:leading-[32px]"
+      className="font-poppins text-[20px] leading-[32px] pt-7 text-center md:text-left sm:mb-[42px] text-[#b1b2b3]  
+           sm:text-[20px] sm:leading-[32px] md:text-"
     >
       A highly motivated and passionate developer with experience in{" "}
       {Bio.languages.map((lang, index) => (
