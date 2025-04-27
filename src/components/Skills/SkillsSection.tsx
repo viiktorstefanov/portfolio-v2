1d51d3a15f828952559f6ef1d22b4616a4ada997
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:41:07 +0300

    edit skills section styles

--- src/components/Skills/SkillsSection.tsx
+++ src/components/Skills/SkillsSection.tsx
@@ -9,7 +9,7 @@ const SkillsSection: React.FC<SkillsSectionProps> = ({ id, children }) => {
   return (
     <section
       id={id}
-      className="flex flex-col justify-center relative z-1 items-center pt-25"
+      className="flex flex-col justify-center relative z-1 items-center pt-25 md:pt-35"
     >
       {children}
     </section>
