75e20c60134a582fac2a625b6fd2122caf0a8682
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-27 19:41:55 +0300

    edit projects section styles

--- src/components/Projects/ProjectsSection.tsx
+++ src/components/Projects/ProjectsSection.tsx
@@ -9,7 +9,7 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({ children, id }) => {
   return (
     <section
       id={id}
-      className="clip-projects-polygon flex flex-col justify-center relative z-[1] items-center pt-20"
+      className="clip-projects-polygon flex flex-col justify-center relative z-[1] items-center pt-20 md:pt-30"
     >
       {children}
     </section>
