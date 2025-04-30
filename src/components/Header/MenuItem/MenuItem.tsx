1ecf208501b1157c541bccd416e0269b4d694740
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-30 13:04:40 +0300

    edit menu item comp

--- src/components/Header/MenuItem/MenuItem.tsx
+++ src/components/Header/MenuItem/MenuItem.tsx
@@ -8,8 +8,22 @@ type MenuItemProps = {
 };
 
 const MenuItem: React.FC<MenuItemProps> = ({ target, label, onClick }) => {
+
+  const isFirefox = typeof navigator !== "undefined" && /Firefox/.test(navigator.userAgent);
+  
+  const scrollDuration = isFirefox ? 1000 : 100;
+
   return (
-    <Link className='font-medium hover:text-primary-app transition duration-200 ease-in-out leading-[0] cursor-pointer' onClick={onClick} to={target} smooth={true} duration={1000} hashSpy={true}>
+    <Link
+    to={target}
+    onClick={onClick}
+    smooth={true}
+    duration={scrollDuration}
+    hashSpy={true}
+    spy={true}
+    activeClass="active"
+    className="font-medium hover:text-primary-app transition duration-200 ease-in-out leading-[0] cursor-pointer"
+    >
       {label}
     </Link>
   );
