e76a3d0c4fafcb78097660cd30663aa921cd69d8
Viktor Stefanov <victorystefanov@gmail.com>
2025-04-30 13:19:08 +0300

    adding example data

---
+++ src/constants/example.data.ts
@@ -0,0 +1,26 @@
+import {
+  BioType,
+  Certificate,
+  Education,
+  Experience,
+  Project,
+  SkillCategory,
+} from "../types/constants";
+
+export const Bio: BioType = {
+};
+
+export const skills: SkillCategory[] = [
+];
+
+export const experiences: Experience[] = [
+];
+
+export const education: Education[] = [
+];
+
+export const certificates: Certificate[] = [
+];
+
+export const projects: Project[] = [
+];
