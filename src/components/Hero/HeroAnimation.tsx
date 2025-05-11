e1d625a8b9c0257557eede3280f8a0c8f6a8a176
Viktor Stefanov <victorystefanov@gmail.com>
2025-05-11 14:46:37 +0300

    edit stroke of hero animation

--- src/components/Hero/HeroAnimation.tsx
+++ src/components/Hero/HeroAnimation.tsx
@@ -15,16 +15,19 @@ const HeroAnimation: React.FC = () => {
             clipRule="evenodd"
             d="M201.337 87.437C193.474 79.5738 180.725 79.5738 172.862 87.437L87.437 172.862C79.5739 180.725 79.5739 193.474 87.437 201.337L400.663 514.563C408.526 522.426 421.275 522.426 429.138 514.563L514.563 429.138C522.426 421.275 522.426 408.526 514.563 400.663L201.337 87.437ZM30.4869 115.912C-8.82897 155.228 -8.82897 218.972 30.4869 258.287L343.713 571.513C383.028 610.829 446.772 610.829 486.088 571.513L571.513 486.088C610.829 446.772 610.829 383.028 571.513 343.713L258.287 30.4869C218.972 -8.82896 155.228 -8.82896 115.912 30.4869L30.4869 115.912Z"
             stroke="url(#paint0_radial)"
+            strokeWidth="3"
             id="path_0"
           />
           <path
             d="M514.563 201.337C522.426 193.474 522.426 180.725 514.563 172.862L429.138 87.437C421.275 79.5738 408.526 79.5739 400.663 87.437L358.098 130.002L301.148 73.0516L343.713 30.4869C383.028 -8.82896 446.772 -8.82896 486.088 30.4869L571.513 115.912C610.829 155.228 610.829 218.972 571.513 258.287L357.802 471.999L300.852 415.049L514.563 201.337Z"
             stroke="url(#paint1_radial)"
+            strokeWidth="3"
             id="path_1"
           />
           <path
             d="M243.901 471.999L201.337 514.563C193.474 522.426 180.725 522.426 172.862 514.563L87.437 429.138C79.5739 421.275 79.5739 408.526 87.437 400.663L301.148 186.952L244.198 130.002L30.4869 343.713C-8.82897 383.028 -8.82897 446.772 30.4869 486.088L115.912 571.513C155.228 610.829 218.972 610.829 258.287 571.513L300.852 528.949L243.901 471.999Z"
             stroke="url(#paint2_radial)"
+            strokeWidth="3"
             id="path_2"
           />
         </g>
@@ -46,6 +49,7 @@ const HeroAnimation: React.FC = () => {
           d="M294.685 193.474L268.932 219.258"
           transform="translate(-294.685 -193.474) rotate(45 294.685 193.474)"
           stroke="url(#paint3_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion dur="10s" repeatCount="indefinite" rotate="auto">
@@ -78,6 +82,7 @@ const HeroAnimation: React.FC = () => {
           d="M294.685 193.474L268.932 219.258"
           transform="translate(-294.685 -193.474) rotate(45 294.685 193.474)"
           stroke="url(#paint7_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion
@@ -109,6 +114,7 @@ const HeroAnimation: React.FC = () => {
           d="M476.171 362.952L450.417 337.168"
           transform="translate(-476.525 -363.313) rotate(-45 476.171 362.952)"
           stroke="url(#paint4_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion dur="10s" repeatCount="indefinite" rotate="auto">
@@ -140,6 +146,7 @@ const HeroAnimation: React.FC = () => {
           d="M381.81 154.669L356.057 128.885"
           transform="translate(-381.81 -154.669) rotate(-45 381.81 154.669)"
           stroke="url(#paint5_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion
@@ -176,6 +183,7 @@ const HeroAnimation: React.FC = () => {
           d="M333.667 382.335L359.42 356.551"
           transform="scale(-1 1) translate(-333.667 -382.335) rotate(45 333.667 382.335)"
           stroke="url(#paint6_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion
@@ -212,6 +220,7 @@ const HeroAnimation: React.FC = () => {
           d="M165.182 94.3159L139.429 120.1"
           transform="translate(-165.182 -94.3159) rotate(45 165.182 94.3159)"
           stroke="url(#paint7_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion
@@ -248,6 +257,7 @@ const HeroAnimation: React.FC = () => {
           d="M476.171 362.952L450.417 337.168"
           transform="translate(-476.525 -363.313) rotate(-45 476.171 362.952)"
           stroke="url(#paint11_linear)"
+          strokeWidth="3"
           opacity="0"
         >
           <animateMotion
